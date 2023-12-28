6d58d4599eeffda71db858e394e931eb97246237
zsh2401 <zsh2401@163.com>
2023-12-28 14:58:15 +0800

    指定设备

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 import torch.cuda
 
 device = "cpu"
@@ -5,3 +7,6 @@ if torch.cuda.is_available():
     device = "cuda"
 elif torch.backends.mps.is_available():
     device = "mps"
+
+if "DEVICE" in os.environ:
+    batch_size = str(os.environ["DEVICE"])
