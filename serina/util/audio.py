a98b214e93c7cf1dc04e458d5ce38b568cce99f1
zsh2401 <zsh2401@163.com>
2024-01-04 15:47:45 +0800

    use resnet34

--- serina/util/audio.py
+++ serina/util/audio.py
@@ -73,7 +73,7 @@ vision_transform = VT.Compose([
     VT.ToPILImage(),
     VT.Lambda(lambda x: x.convert('RGB')),
     # VT.Resize((224, 224)),
-    VT.Resize((224, 224)),
+    VT.Resize((224, 224 * 6)),
     VT.ToTensor(),  # 将图片转换为Tensor
     VT.Normalize(mean=[0.485, 0.456, 0.406],  # 图像标准化
                  std=[0.229, 0.224, 0.225])
